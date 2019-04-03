63d4ad488c69d9502c65c5f7de6f35360a7b14f0
Nicole Laczny <nlaczn2@uic.edu>
2019-04-03 13:37:09 -0600

    Signed-off-by: Nicole Laczny <nlaczn2@uic.edu>

---
+++ ZombiesVsWizard_UnityProject/Assets/Scripts/necrosttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class necrosttack : MonoBehaviour
+{
+
+    Rigidbody rb;
+    bool PlayerPresent = false, ZombiesPresent = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float distance;
+
+        float angle = 0;
+        for (int i = 0; i < 30; i++)
+        {
+            float x = Mathf.Sin(angle);
+            float y = Mathf.Cos(angle);
+            angle += 10 * Mathf.PI;
+
+            Vector3 dir = new Vector3(transform.position.x + angle, transform.position.y, 0);
+            RaycastHit hit;
+            Debug.DrawLine(transform.position, dir, Color.red);
+            if (Physics.Raycast(transform.position, dir, out hit))
+            {
+                print(hit.collider.gameObject.name);
+                if (hit.collider.gameObject.name.Equals("Player"))
+                {
+                    PlayerPresent = true;
+                    distance = hit.distance;
+                    print(hit.collider.gameObject.name); //wizard3?
+                    float step = -2f * Time.deltaTime;
+                    transform.position = Vector3.MoveTowards(transform.position, hit.collider.gameObject.transform.position, step);
+
+                }
+                if (hit.collider.gameObject.name.ToUpper().Contains("ZOMBIE"))
+                {
+                    ZombiesPresent = true;
+                    if (PlayerPresent && ZombiesPresent)
+                    {
+                        float step = -2f * Time.deltaTime; // calculate distance to move
+                                                           //hit.collider.gameObject.transform.position = Vector3.MoveTowards(hit.collider.gameObject.transform.position, transform.position, step);
+                        transform.position = Vector3.MoveTowards(transform.position, hit.collider.gameObject.transform.position, step);
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ ZombiesVsWizard_UnityProject/Assets/Scripts/rayforward.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rayforward : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float distance;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 15; //change 15 units to length of room for NPC2
+        Debug.DrawRay(transform.position, forward, Color.cyan);
+        if (Physics.Raycast(transform.position, forward, out RaycastHit hit))
+        {
+            if (hit.collider.gameObject.name.Equals("Player"))
+            {
+                distance = hit.distance;
+                print(hit.collider.gameObject.name); //wizard3?
+                float step = 1f * Time.deltaTime; // calculate distance to move
+                transform.position = Vector3.MoveTowards(transform.position, hit.collider.gameObject.transform.position, step);
+                //name is hit.collider.gameObject.name
+            }
+        }
+    }
+}
