66e38126c3b579dc57c65a9378fe4c897599215a
Nicole Laczny <nlaczn2@uic.edu>
2019-04-16 21:45:45 -0500

    Signed-off-by: Nicole Laczny <nlaczn2@uic.edu>

---
+++ ZombiesVsWizard_UnityProject/Assets/talk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talk : MonoBehaviour
+{
+    public GameObject startT;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            startT.SetActive(false);
+
+        }
+    }
+}
