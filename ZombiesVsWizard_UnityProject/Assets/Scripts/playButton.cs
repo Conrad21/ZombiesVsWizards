17b738b0eda76b622d0a68cd18af63057ef382f4
Nicole Laczny <nlaczn2@uic.edu>
2019-04-16 19:48:47 -0500

    Signed-off-by: Nicole Laczny <nlaczn2@uic.edu>

--- ZombiesVsWizard_UnityProject/Assets/Scripts/playButton.cs
+++ ZombiesVsWizard_UnityProject/Assets/Scripts/playButton.cs
@@ -5,12 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class playButton : MonoBehaviour
 {
-    public AudioClip sound;
-    public AudioSource source;
+    public AudioClip Sound;
+    public AudioSource Source;
+
+    public AudioClip WhooshSound;
+    public AudioSource WhooshSource;
 
     public void ButtonPress()
     {
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        WhooshSource.PlayOneShot(Sound);
+
+        if (WhooshSource.isPlaying == false)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Source.isPlaying == false)
+        {
+            Source.PlayOneShot(Sound);
+        }
+
+
     }
 }
