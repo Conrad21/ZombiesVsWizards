e03f96f216c3009f349032fcc162ab58ad1383e8
Nicole Laczny <nlaczn2@uic.edu>
2019-04-17 16:38:39 -0500

    Signed-off-by: Nicole Laczny <nlaczn2@uic.edu>

---
+++ ZombiesVsWizard_UnityProject/Assets/vanish.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vanish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected");
+        Destroy(gameObject);
+    }
+}
