397ee5d11c05d2c7522dcb44a715f59ac19744b2
Conrad Ptasznik <siggamesuic@gmail.com>
2019-04-23 17:03:52 -0500

    stuff

---
+++ ZombiesVsWizard_UnityProject/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
+    public int attackDamage = 10;               // The amount of health taken away per attack.
+
+  
+
+    public GameObject player ;                          // Reference to the player GameObject.
+    PlayerHealth playerHealth;                  // Reference to the player's health.
+   // EnemyHealth enemyHealth;                    // Reference to this enemy's health.
+    bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
+    float timer;                                // Timer for counting up to the next attack.
+
+
+    void Start ()
+    {
+        // Setting up the references.
+        //player = GameObject.FindWithTag("MyPlayer");
+         player = GameObject.FindWithTag("MyPlayer");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+      //  enemyHealth = GetComponent<EnemyHealth>();
+
+    }
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        // If the entering collider is the player...
+        if(other.gameObject == player)
+        {
+            // ... the player is in range.
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit (Collider other)
+    {
+        // If the exiting collider is the player...
+        if(other.gameObject == player)
+        {
+            // ... the player is no longer in range.
+            playerInRange = false;
+        }
+    }
+
+
+    void Update ()
+    {
+        // Add the time since Update was last called to the timer.
+        timer += Time.deltaTime;
+
+        // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+        if(timer >= timeBetweenAttacks && playerInRange )
+        {
+            // ... attack.
+            Attack ();
+        }
+
+        // If the player has zero or less health...
+        
+    }
+
+
+    void Attack ()
+    {
+        // Reset the timer.
+        timer = 0f;
+
+        // If the player has health to lose...
+        if(playerHealth.CurrentHealth > 0)
+        {
+            // ... damage the player.
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+}
---
+++ ZombiesVsWizard_UnityProject/Assets/Scripts/playerHealth.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth: MonoBehaviour
+{
+    public int tacos; 
+    public float CurrentHealth {get; set; }
+    public float MaxHealth {get; set;}
+    public Slider healthbar; 
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    public float flashSpeed = 5f;  
+    bool isDead;                                             
+    bool damaged;       
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHealth = 20f; 
+        CurrentHealth = MaxHealth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.P)){
+             TakeDamage(6);
+             CurrentHealth = (CurrentHealth - 6);
+        }
+
+        //  if(damaged)
+        // {
+        //     // ... set the colour of the damageImage to the flash colour.
+        //     //damageImage.color = flashColour;
+        // }
+        // else
+        // {
+        //     // ... transition the colour back to clear.
+        //   //  damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        // }
+
+        // damaged = false;
+    }
+    public void TakeDamage(float damageValue){
+        damaged = true;
+        CurrentHealth = (CurrentHealth - damageValue); 
+        healthbar.value = CalculateHealth(); 
+        
+        if(CurrentHealth <= 0){
+        Die();
+        }
+    }
+    float CalculateHealth(){
+        return CurrentHealth/MaxHealth ; 
+    }
+
+
+    void Die(){
+        CurrentHealth = 0;
+        Debug.Log("You have Dead!"); 
+        SceneManager.LoadScene( 2);
+    }
+}
